fc54e4114941f402184ebfd8c1c1f2b47c547156
jin92926 <jin92926@gmail.com>
2022-08-08 13:46:34 +0900

    1차 수정

---
+++ my-agora-states-client/src/App.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import App from './App'
+
+describe('test입니다', () => {
+    test('2+2는 4', () => {
+        expect(2 + 2).toBe(4);
+    });
+    test('3+3는 6', () => {
+        expect(3 + 3).toBe(6);
+    });
+    it('My Agora States 텍스트를 가지고 있어야 한다', () => {
+        render(<App />);
+        //단순히 가지고 있는 텍스트로 dom에 접근하는 방식인 getByText을 사용
+        screen.getByText(/My agora States/i);
+   
+    })
+
+    test('제목은 My agora States이어야 한다', () => {
+    render(<App />);
+    screen.getByText(/My agora States1/i);
+   
+    });
+})
